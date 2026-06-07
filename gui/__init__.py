0bfbf543dede75231f2559964859ab2f877b3dbe
GGDoggy <ted.huang225@gmail.com>
2026-06-07 21:19:40 +0800

    Remove gui/

--- gui/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Public package exports for the interactive Panel dashboard."""
-
-from .dashboard import OrderbookDashboard
-from .webUI import build_app, main
-
-__all__ = ["OrderbookDashboard", "build_app", "main"]
